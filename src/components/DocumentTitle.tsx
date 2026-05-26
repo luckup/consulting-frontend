5ec3ed9cbd88e3f8f0d92df92cd1133156de3698
thombsjen <jaconsult05@gmail.com>
2026-05-26 11:36:44 -0700

    add seo feat and google analytics

--- src/components/DocumentTitle.tsx
+++ src/components/DocumentTitle.tsx
@@ -1,21 +1,12 @@
 import { useEffect } from 'react'
 import { useLocation } from 'react-router-dom'
-import { getRouteMeta } from '@/lib/routeMeta'
+import { getRouteMeta, setPageMeta } from '@/lib/routeMeta'
 
 export function DocumentTitle() {
   const { pathname } = useLocation()
 
   useEffect(() => {
-    const meta = getRouteMeta(pathname)
-    document.title = meta.title
-
-    let description = document.querySelector<HTMLMetaElement>('meta[name="description"]')
-    if (!description) {
-      description = document.createElement('meta')
-      description.name = 'description'
-      document.head.appendChild(description)
-    }
-    description.content = meta.description
+    setPageMeta(getRouteMeta(pathname), pathname)
   }, [pathname])
 
   return null
